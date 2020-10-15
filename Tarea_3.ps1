8439b42fba51a4d0211673ceb9ca4901caf5554b
KassGP <72727108+KassGP@users.noreply.github.com>
2020-10-14 20:39:23 -0500

    Add files via upload

---
+++ Tarea_3.ps1
@@ -0,0 +1,48 @@
+﻿function cuadrado{
+    $re = $n*$n
+    "Cuadrado: "+$re
+}
+function cubo{
+    $re = [math]::Pow($n, 3)
+    "Cubo: "+$re
+}
+ function raiz{
+    $re = [math]::Sqrt($n)
+    "Raiz: "+$re
+}
+clear
+function opciones{
+    Write-Host "---- Opciones ----"
+    Write-Host "Seleccione la operación a realizar"
+    Write-Host "Presione 1 para elevarlo al cuadrado"
+    Write-Host "Presione 2 para elevarlo al cubo"
+    Write-Host "Presione 3 para sacar su raiz"
+    Write-Host "Presione 0 para salir"
+}
+do{
+    "Operaciones"
+    $n = read-Host "Ingrese un numero"
+    Opciones
+    $input = Read-Host "Seleccione la opcion que desee"
+    clear
+    switch($input){
+        '1'{
+            cuadrado
+            
+        }
+        '2'{
+            cubo
+              
+        }
+        '3'{
+            raiz
+            
+        }'0'{
+            
+            $quit = "0"
+        }
+    }
+    
+}Until ($quit -eq "0")
+clear
+Write-Host "Nos vemos :)"
